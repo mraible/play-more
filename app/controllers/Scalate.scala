13851221ed05e06119af486d0d41045f84cd343f
Matt Raible <matt@raibledesigns.com>
2011-10-27 00:56:13 -0700

    Added support for specifying template by name.

--- app/controllers/Scalate.scala
+++ app/controllers/Scalate.scala
@@ -1,11 +1,19 @@
 package controllers
 
-import play.mvc.Http
+import play.mvc.{Scope, Http}
 
 trait Scalate {
 
   def render(args: (Symbol, Any)*) = {
-    def defaultTemplate = Http.Request.current().action.replace(".", "/")
-    ScalateTemplate(defaultTemplate).render(args: _*);
+    var template = Scope.RenderArgs.current().get("template")
+    if (template == null) {
+      template = Http.Request.current().action.replace(".", "/")
+    }
+
+    renderTemplate(template.toString, args: _*);
+  }
+
+  def renderTemplate(template: String, args: (Symbol, Any)*) = {
+    ScalateTemplate(template).render(args: _*);
   }
 }
