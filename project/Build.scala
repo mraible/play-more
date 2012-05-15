6ba744090b59e42f5572fe5f027c8404094d1512
Matt Raible <matt@raibledesigns.com>
2012-05-15 11:00:18 -0600

    Created new Play 2 app and evolutions to create database.

---
+++ project/Build.scala
@@ -0,0 +1,18 @@
+import sbt._
+import Keys._
+import PlayProject._
+
+object ApplicationBuild extends Build {
+
+    val appName         = "play-more"
+    val appVersion      = "1.0-SNAPSHOT"
+
+    val appDependencies = Seq(
+      "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
+    )
+
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+      // Add your own project settings here      
+    )
+
+}
---
+++ test/EvolutionsTest.scala
@@ -0,0 +1,27 @@
+import org.specs2.mutable._
+
+import play.api.db.DB
+import play.api.Play.current
+
+import anorm._
+import anorm.SqlParser._
+
+import play.api.test._
+import play.api.test.Helpers._
+
+class EvolutionsTest extends Specification {
+
+  "Evolutions" should {
+    "be applied without errors" in {
+      evolutionFor("default")
+      running(FakeApplication()) {
+        DB.withConnection {
+          implicit connection =>
+            SQL("select count(1) from athlete").execute()
+            SQL("select count(1) from workout").execute()
+        }
+      }
+      success
+    }
+  }
+}
