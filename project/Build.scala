a2e07d4c8bf85a04fe845028c377b86652ed245a
Matt Raible <matt@raibledesigns.com>
2012-06-01 08:04:57 -0600

    Removed ScalaTest as a dependency.

--- project/Build.scala
+++ project/Build.scala
@@ -8,13 +8,11 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
-      "org.scalatest" %% "scalatest" % "1.7.2" % "test"
+      "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       // Add your own project settings here
-      testOptions in Test := Nil // disables specs2
     )
 
 }
