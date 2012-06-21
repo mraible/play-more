3e981ab4f31c6333ee0276cd2d84b44cba47b7c8
Matt Raible <matt@raibledesigns.com>
2012-06-21 13:37:23 -0600

    Changed from absolute path to canonical path. Fixes #11 on Heroku.

--- app/controllers/Scalate.scala
+++ app/controllers/Scalate.scala
@@ -21,7 +21,7 @@ object Scalate {
     val engine = new TemplateEngine
     engine.resourceLoader = new FileResourceLoader(Some(Play.getFile("app/views")))
     engine.layoutStrategy = new DefaultLayoutStrategy(engine,
-      Play.getFile("app/views/layouts/default." + format).getAbsolutePath)
+      Play.getFile("app/views/layouts/default." + format).getCanonicalPath)
     engine.classpath = Play.getFile("/tmp/classes").getAbsolutePath
     engine.workingDirectory = Play.getFile("tmp")
     engine.combinedClassPath = true
