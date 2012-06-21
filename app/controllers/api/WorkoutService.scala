f2cee049bb9da5695ee912d44faf586739cdfed2
Matt Raible <matt@raibledesigns.com>
2012-06-20 22:10:21 -0600

    Allow debugging with Safari from remote host.

--- app/controllers/api/WorkoutService.scala
+++ app/controllers/api/WorkoutService.scala
@@ -8,10 +8,26 @@ import org.codehaus.jackson.JsonNode
 object WorkoutService extends Controller  {
 
   def workouts = Action { implicit request =>
-    var json = Json.generate(Workout.allWithAthlete)
-    // todo: figure out regex to replace passwords in json
-    json = json.replaceAll("beer|whiskey", "XXX")
-    Ok(json).as("application/json")
+    // Necessary if you want to run mobile app in local browser
+    if (request.method == "OPTIONS") {
+      println("OPTIONS")
+      Ok.withHeaders(
+        "Access-Control-Allow-Origin" -> "*",
+        "Access-Control-Allow-Methods" -> "GET,POST",
+        "Access-Control-Max-Age" -> "360",
+        "Access-Control-Allow-Headers" -> "x-requested-with"
+      )
+    } else {
+      var json = Json.generate(Workout.allWithAthlete)
+      // todo: figure out regex to replace passwords in json
+      json = json.replaceAll("beer|whiskey", "XXX")
+      Ok(json).as("application/json").withHeaders(
+        "Access-Control-Allow-Origin" -> "*",
+        "Access-Control-Allow-Methods" -> "GET,POST",
+        "Access-Control-Max-Age" -> "360",
+        "Access-Control-Allow-Headers" -> "x-requested-with"
+      )
+    }
   }
 
   def show(id: Long) = Action {
